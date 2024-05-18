d233c00e20473a7fac45c620994e7fa090e330b0
dolong2 <k01066624566@gmail.com>
2024-05-18 14:53:35 +0900

    [chore] root 커맨드 부연 설명 수정

--- cmd/root.go
+++ cmd/root.go
@@ -29,17 +29,19 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "dcd_cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "dcd-cli",
+	Short: "CLI for DCD Service",
+	Long: `#####      ####   #####               ####    ###       ##                         ##
+## ##    ##  ##   ## ##             ##  ##    ##                                  ##
+##  ##  ##        ##  ##           ##         ##      ###      ####    #####     #####
+##  ##  ##        ##  ##           ##         ##       ##     ##  ##   ##  ##     ##
+##  ##  ##        ##  ##           ##         ##       ##     ######   ##  ##     ##
+## ##    ##  ##   ## ##             ##  ##    ##       ##     ##       ##  ##     ## ##
+#####      ####   #####               ####    ####     ####     #####   ##  ##      ###
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+DCD CLI is client for DCD Service.
+For more information, please visit https://github.com/dolong2/dcd-client
+`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -52,13 +54,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dcd_cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
