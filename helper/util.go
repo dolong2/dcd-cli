c3d37ce365709163ecd598a20cba928e3aea261d
dolong2 <k01066624566@gmail.com>
2024-05-28 23:36:16 +0900

    [add] GetAccessToken 메서드 추가

---
+++ helper/util.go
@@ -0,0 +1,61 @@
+package helper
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
+
+func GetAccessToken() (string, error) {
+	type tokenResponse struct {
+		AccessToken     string    `json:"accessToken"`
+		RefreshToken    string    `json:"refreshToken"`
+		AccessTokenExp  time.Time `json:"accessTokenExp"`
+		RefreshTokenExp time.Time `json:"refreshTokenExp"`
+	}
+
+	const timeFormat = "2006-01-02T15:04:05"
+
+	tokenInfoFile, err := os.Open("./dcd-info/token-info.json")
+	if err != nil {
+		return "", err
+	}
+	defer tokenInfoFile.Close()
+
+	decoder := json.NewDecoder(tokenInfoFile)
+	var raw map[string]interface{}
+	err = decoder.Decode(&raw)
+	if err != nil {
+		return "", err
+	}
+
+	// 커스텀 시간 형식 파싱
+	accessTokenExp, err := time.Parse(timeFormat, raw["accessTokenExp"].(string))
+	if err != nil {
+		fmt.Println("AccessTokenExp 파싱 중 오류 발생:", err)
+		return "", err
+	}
+	refreshTokenExp, err := time.Parse(timeFormat, raw["refreshTokenExp"].(string))
+	if err != nil {
+		fmt.Println("RefreshTokenExp 파싱 중 오류 발생:", err)
+		return "", err
+	}
+
+	// AccessToken이 만료되었을때 토큰 재발급
+	now := time.Now()
+	if now.After(accessTokenExp) {
+		//TODO 토큰 재발급 실행
+	} else if now.After(refreshTokenExp) {
+		return "", errors.New("login info is expired.\nplease retry login.")
+	}
+
+	tokenInfo := tokenResponse{
+		AccessToken:     raw["accessToken"].(string),
+		RefreshToken:    raw["refreshToken"].(string),
+		AccessTokenExp:  accessTokenExp,
+		RefreshTokenExp: refreshTokenExp,
+	}
+	return tokenInfo.AccessToken, nil
+}
