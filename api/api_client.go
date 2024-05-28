a32770c2fea8c8e162bac773a60c5159e81d0032
dolong2 <k01066624566@gmail.com>
2024-05-28 23:45:50 +0900

    [add] SendPatch 메서드 추가

--- api/api_client.go
+++ api/api_client.go
@@ -51,3 +51,40 @@ func SendPost(targetUrl string, header map[string]string, body []byte) ([]byte, 
 
 	return result, nil
 }
+
+func SendPatch(targetUrl string, header map[string]string, body []byte) ([]byte, error) {
+	httpClient := &http.Client{}
+	request, err := http.NewRequest("PATCH", baseUrl+targetUrl, bytes.NewBuffer(body))
+	if err != nil {
+		return []byte(""), err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+
+	httpResponse, err := httpClient.Do(request)
+	if err != nil {
+		return []byte(""), err
+	}
+	defer httpResponse.Body.Close()
+
+	// 200번대 응답코드가 아닐때 에러
+	if httpResponse.StatusCode/100 != 2 {
+		rawErrorResponse, err := io.ReadAll(httpResponse.Body)
+		if err != nil {
+			return []byte(""), err
+		}
+		errorResponse := apiErrorResponse{}
+		json.Unmarshal(rawErrorResponse, &errorResponse)
+		return []byte(""), httpErr.NewHttpError(errorResponse.Status, errorResponse.Message)
+	}
+
+	result, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return result, nil
+}
