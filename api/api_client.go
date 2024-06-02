be53639ffef6f8173c9e3ffe5089318d755509c8
dolong2 <k01066624566@gmail.com>
2024-06-02 17:51:10 +0900

    [add] SendGet 메서드 추가

--- api/api_client.go
+++ api/api_client.go
@@ -15,6 +15,43 @@ type apiErrorResponse struct {
 	Message string `json:"message"`
 }
 
+func SendGet(targetUrl string, header map[string]string) ([]byte, error) {
+	httpClient := &http.Client{}
+	request, err := http.NewRequest("GET", baseUrl+targetUrl, bytes.NewBuffer([]byte("")))
+	if err != nil {
+		return []byte(""), err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+
+	httpResponse, err := httpClient.Do(request)
+	if err != nil {
+		return []byte(""), err
+	}
+	defer httpResponse.Body.Close()
+
+	// 200번대 응답코드가 아닐때 에러
+	if httpResponse.StatusCode/100 != 2 {
+		rawErrorResponse, err := io.ReadAll(httpResponse.Body)
+		if err != nil {
+			return []byte(""), err
+		}
+		errorResponse := apiErrorResponse{}
+		json.Unmarshal(rawErrorResponse, &errorResponse)
+		return []byte(""), httpErr.NewHttpError(errorResponse.Status, errorResponse.Message)
+	}
+
+	result, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return result, nil
+}
+
 func SendPost(targetUrl string, header map[string]string, body []byte) ([]byte, error) {
 	httpClient := &http.Client{}
 	request, err := http.NewRequest("POST", baseUrl+targetUrl, bytes.NewBuffer(body))
