baa5ed343f0ce74b71e628a094f708ec1baf7004
dolong2 <k01066624566@gmail.com>
2024-06-04 23:44:37 +0900

    [chore] id 플래그가 존재할때 단일 워크스페이스만 조회하도록 수정

--- cmd/workspace.go
+++ cmd/workspace.go
@@ -12,11 +12,20 @@ var workspacesCmd = &cobra.Command{
 	Short: "sub command to get workspaces",
 	Long:  `this command can be used to get workspaces`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err != nil {
-			fmt.Println(err)
-		}
-		for _, workspace := range workspaceList.List {
+		id, existsFlagErr := cmd.Flags().GetString("id")
+		if id == "" || existsFlagErr != nil {
 			workspaceList, err := exec.GetWorkspaces()
+			if err != nil {
+				fmt.Println(err)
+			}
+			for _, workspace := range workspaceList.List {
+				fmt.Printf("ID: %s\nTitle: %s\nDescription: %s\n\n", workspace.Id, workspace.Title, workspace.Description)
+			}
+		} else {
+			workspace, err := exec.GetWorkspace(id)
+			if err != nil {
+				fmt.Println(err)
+			}
 			fmt.Printf("ID: %s\nTitle: %s\nDescription: %s\n\n", workspace.Id, workspace.Title, workspace.Description)
 		}
 	},
