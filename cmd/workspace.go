6d664df45a08daad4f952871d1fd4d2bea46ae1a
dolong2 <k01066624566@gmail.com>
2024-06-06 09:45:12 +0900

    [chore] workspaces 서브 커맨드에서 에러 발생시 cmdError를 발생시키도록 수정

--- cmd/workspace.go
+++ cmd/workspace.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/dolong2/dcd-cli/api/exec"
+	cmdError "github.com/dolong2/dcd-cli/cmd/err"
 	"github.com/spf13/cobra"
 )
 
@@ -11,12 +12,12 @@ var workspacesCmd = &cobra.Command{
 	Use:   "workspaces",
 	Short: "sub command to get workspaces",
 	Long:  `this command can be used to get workspaces`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		id, existsFlagErr := cmd.Flags().GetString("id")
 		if id == "" || existsFlagErr != nil {
 			workspaceList, err := exec.GetWorkspaces()
 			if err != nil {
-				fmt.Println(err)
+				return cmdError.NewCmdError(1, err.Error())
 			}
 			for _, workspace := range workspaceList.List {
 				fmt.Printf("ID: %s\nTitle: %s\nDescription: %s\n\n", workspace.Id, workspace.Title, workspace.Description)
@@ -24,10 +25,12 @@ var workspacesCmd = &cobra.Command{
 		} else {
 			workspace, err := exec.GetWorkspace(id)
 			if err != nil {
-				fmt.Println(err)
+				return cmdError.NewCmdError(1, err.Error())
 			}
 			fmt.Printf("ID: %s\nTitle: %s\nDescription: %s\n\n", workspace.Id, workspace.Title, workspace.Description)
 		}
+
+		return nil
 	},
 }
 
