e13b3e5e934967737c98b34be70ef77a1250f3fa
dolong2 <k01066624566@gmail.com>
2024-06-08 11:50:34 +0900

    [add] env 추가 요청 로직 추가

---
+++ api/exec/add_env.go
@@ -0,0 +1,35 @@
+package exec
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dolong2/dcd-cli/api"
+)
+
+type envRequest struct {
+	EnvList map[string]string `json:"envList"`
+}
+
+func AddEnv(applicationId string, key string, value string) error {
+	header := make(map[string]string)
+	accessToken, err := GetAccessToken()
+	if err != nil {
+		return err
+	}
+	header["Authorization"] = "Bearer " + accessToken
+
+	body := map[string]string{}
+	body[key] = value
+	envReq := envRequest{EnvList: body}
+	requestJson, err := json.Marshal(envReq)
+
+	fmt.Println(envReq)
+	fmt.Println(string(requestJson))
+
+	_, err = api.SendPost("/application/"+applicationId+"/env", header, requestJson)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
