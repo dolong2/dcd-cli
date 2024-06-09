15689b073058a9a780178e7bcad855cdfc3ce384
dolong2 <k01066624566@gmail.com>
2024-06-09 15:01:54 +0900

    [add] sendHttpReq 메서드 추가

--- api/api_client.go
+++ api/api_client.go
@@ -178,3 +178,47 @@ func SendPatch(targetUrl string, header map[string]string, body []byte) ([]byte,
 
 	return result, nil
 }
+
+func sendHttpReq(method string, targetUrl string, header map[string]string, param []byte, body []byte) ([]byte, error) {
+	if len(param) != 0 {
+		targetUrl += "?"
+	}
+	for key, value := range param {
+		targetUrl += fmt.Sprintf("%s=%s&", key, value)
+	}
+
+	httpClient := &http.Client{}
+	request, err := http.NewRequest(method, baseUrl+targetUrl, bytes.NewBuffer(body))
+	if err != nil {
+		return []byte(""), err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+
+	httpResponse, err := httpClient.Do(request)
+	if err != nil {
+		return []byte(""), err
+	}
+	defer httpResponse.Body.Close()
+
+	// 200번대 응답코드가 아닐때 에러
+	if httpResponse.StatusCode/100 != 2 {
+		rawErrorResponse, err := io.ReadAll(httpResponse.Body)
+		if err != nil {
+			return []byte(""), err
+		}
+		errorResponse := apiErrorResponse{}
+		json.Unmarshal(rawErrorResponse, &errorResponse)
+		return []byte(""), httpErr.NewHttpError(errorResponse.Status, errorResponse.Message)
+	}
+
+	result, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return result, nil
+}
