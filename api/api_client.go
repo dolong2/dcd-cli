1493a1708318b05089206e80d0b71d7c782abec9
dolong2 <k01066624566@gmail.com>
2024-06-09 14:07:53 +0900

    [add] delete 요청 메서드 추가

--- api/api_client.go
+++ api/api_client.go
@@ -97,6 +97,51 @@ func SendPost(targetUrl string, header map[string]string, body []byte) ([]byte, 
 	return result, nil
 }
 
+func SendDelete(targetUrl string, header map[string]string, param map[string]string) ([]byte, error) {
+	httpClient := &http.Client{}
+
+	if len(param) != 0 {
+		targetUrl += "?"
+	}
+	for key, value := range param {
+		targetUrl += fmt.Sprintf("%s=%s&", key, value)
+	}
+
+	request, err := http.NewRequest("DELETE", baseUrl+targetUrl, bytes.NewBuffer([]byte("")))
+	if err != nil {
+		return []byte(""), err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+
+	httpResponse, err := httpClient.Do(request)
+	if err != nil {
+		return []byte(""), err
+	}
+	defer httpResponse.Body.Close()
+
+	// 200번대 응답코드가 아닐때 에러
+	if httpResponse.StatusCode/100 != 2 {
+		rawErrorResponse, err := io.ReadAll(httpResponse.Body)
+		if err != nil {
+			return []byte(""), err
+		}
+		errorResponse := apiErrorResponse{}
+		json.Unmarshal(rawErrorResponse, &errorResponse)
+		return []byte(""), httpErr.NewHttpError(errorResponse.Status, errorResponse.Message)
+	}
+
+	result, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return result, nil
+}
+
 func SendPatch(targetUrl string, header map[string]string, body []byte) ([]byte, error) {
 	httpClient := &http.Client{}
 	request, err := http.NewRequest("PATCH", baseUrl+targetUrl, bytes.NewBuffer(body))
