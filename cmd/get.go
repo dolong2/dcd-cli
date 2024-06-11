4f1db472a7f476fb0d428146163cb92e4af2976f
dolong2 <k01066624566@gmail.com>
2024-06-11 23:02:43 +0900

    [add] get 커맨드에서 사용하는 getApplication 메서드 추가

--- cmd/get.go
+++ cmd/get.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/dolong2/dcd-cli/api/exec"
+	cmdError "github.com/dolong2/dcd-cli/cmd/err"
 	"github.com/spf13/cobra"
 )
 
@@ -36,6 +39,32 @@ func getWorkspace(cmd *cobra.Command) error {
 	}
 	return nil
 }
+
+func getApplication(cmd *cobra.Command) (error, bool) {
+	workspaceId, existsWorkspaceId := cmd.Flags().GetString("workspace")
+	applicationId, existsApplicationId := cmd.Flags().GetString("id")
+	if (existsWorkspaceId != nil || workspaceId == "") && (existsApplicationId != nil || applicationId == "") {
+		return cmdError.NewCmdError(1, "requires workspace flag to get your applications"), true
+	}
+	if workspaceId != "" && applicationId != "" {
+		return cmdError.NewCmdError(125, "simultaneous use of workspaceId and application is not permitted"), true
+	}
+	if workspaceId != "" {
+		applicationListResponse, err := exec.GetApplications(workspaceId)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error()), true
+		}
+		for _, application := range applicationListResponse.Applications {
+			printApplication(application)
+		}
+	} else if applicationId != "" {
+		applicationResponse, err := exec.GetApplication(applicationId)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error()), true
+		}
+		printApplication(*applicationResponse)
+	}
+	return nil, false
 }
 
 func init() {
