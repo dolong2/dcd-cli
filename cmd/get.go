572f4893cb8944d7c2fa3550117f9e4de0bc523b
dolong2 <k01066624566@gmail.com>
2024-06-11 23:03:09 +0900

    [add] 애플리케이션에서 사용할 printApplication 메서드 추가

--- cmd/get.go
+++ cmd/get.go
@@ -70,3 +70,22 @@ func getApplication(cmd *cobra.Command) (error, bool) {
 func init() {
 	rootCmd.AddCommand(getCmd)
 }
+
+func printApplication(application exec.ApplicationResponse) {
+	fmt.Printf("ID: %s\n", application.Id)
+	fmt.Printf("Name: %s\n", application.Name)
+	fmt.Printf("Description: %s\n", application.Description)
+	fmt.Printf("Application Type: %s\n", application.ApplicationType)
+	fmt.Printf("GitHub URL: %s\n", application.GithubUrl)
+	fmt.Printf("Environment Variables: [\n")
+	for key, value := range application.Env {
+		fmt.Printf("  %s: %s\n", key, value)
+	}
+	fmt.Printf("]\n")
+	fmt.Printf("Port: %d\n", application.Port)
+	fmt.Printf("External Port: %d\n", application.ExternalPort)
+	fmt.Printf("Version: %s\n", application.Version)
+	fmt.Printf("Status: %s\n", application.Status)
+	fmt.Println()
+	fmt.Println()
+}
