4fd6730cfc6e0b8a7f5534f04ddbbe5701e68a50
dolong2 <k01066624566@gmail.com>
2024-06-12 23:22:14 +0900

    [add] env-update 커맨드 추가

---
+++ cmd/update.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"github.com/dolong2/dcd-cli/api/exec"
+	cmdError "github.com/dolong2/dcd-cli/cmd/err"
+
+	"github.com/spf13/cobra"
+)
+
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update <applicationId> [flags]",
+	Short: "use to update an application env",
+	Long:  `this command can be used to update a env to an application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return cmdError.NewCmdError(1, "must specify applicationId")
+		}
+		application := args[0]
+		key, existsKey := cmd.Flags().GetString("key")
+		value, existsValue := cmd.Flags().GetString("value")
+		if key == "" || value == "" || existsKey != nil || existsValue != nil {
+			return cmdError.NewCmdError(1, "this command needs to specify both and key and value")
+		}
+		err := exec.UpdateEnv(application, key, value)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error())
+		}
+		return nil
+	},
+}
+
+func init() {
+	envCmd.AddCommand(updateCmd)
+
+	updateCmd.Flags().StringP("key", "k", "", "environment key")
+	updateCmd.Flags().StringP("value", "v", "", "environment value")
+}
