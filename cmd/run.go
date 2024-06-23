abe5dfc93eb618acda199881de06946c8fe21fef
dolong2 <k01066624566@gmail.com>
2024-06-23 21:03:22 +0900

    [add] applicationId를 받아서 run api를 호출하는 로직 추가

--- cmd/run.go
+++ cmd/run.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"fmt"
+	"github.com/dolong2/dcd-cli/api/exec"
+	cmdError "github.com/dolong2/dcd-cli/cmd/err"
 
 	"github.com/spf13/cobra"
 )
@@ -11,8 +12,16 @@ var runCmd = &cobra.Command{
 	Use:   "run <applicationId>",
 	Short: "command to run an application",
 	Long:  `this command is used to run an application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return cmdError.NewCmdError(1, "must specify applicationId")
+		}
+		applicationId := args[0]
+		err := exec.RunApplication(applicationId)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error())
+		}
+		return nil
 	},
 }
 
