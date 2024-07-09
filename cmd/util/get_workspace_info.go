2238cc8ebedf1feef1559ef2893158047caa80dc
dolong2 <k01066624566@gmail.com>
2024-07-09 20:44:29 +0900

    [add] workspaceInfo 혹은 플래그에서 workspaceId를 가져오는 메서드 추가

--- cmd/util/get_workspace_info.go
+++ cmd/util/get_workspace_info.go
@@ -2,21 +2,35 @@ package util
 
 import (
 	"encoding/json"
+	"errors"
 	cmdError "github.com/dolong2/dcd-cli/cmd/err"
+	"github.com/spf13/cobra"
 	"os"
 )
 
-func GetWorkspaceId() (string, error) {
+func GetWorkspaceId(cmd *cobra.Command) (string, error) {
+	workspaceId, err := getWorkspaceInfo()
+	if err != nil {
+		workspaceFlag, err := cmd.Flags().GetString("workspace")
+		if workspaceFlag != "" || err != nil {
+			return "", cmdError.NewCmdError(1, "must specify workspace id")
+		}
+		workspaceId = workspaceFlag
+	}
+	return workspaceId, nil
+}
+
+func getWorkspaceInfo() (string, error) {
 	rawWorkspaceInfo, err := os.ReadFile("./dcd-info/workspace-info.json")
 	if err != nil {
-		return "", cmdError.NewCmdError(125, "not found workspace info")
+		return "", errors.New("not found workspace info")
 	}
 
 	var simpleWorkspaceInfo SimpleWorkspaceInfo
 
 	err = json.Unmarshal(rawWorkspaceInfo, &simpleWorkspaceInfo)
 	if err != nil {
-		return "", cmdError.NewCmdError(125, "invalid workspace info")
+		return "", errors.New("invalid workspace info")
 	}
 
 	return simpleWorkspaceInfo.WorkspaceId, nil
