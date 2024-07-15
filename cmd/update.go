228ad31c7e64d826011e137426e18d5c0da12e82
dolong2 <k01066624566@gmail.com>
2024-07-15 20:23:06 +0900

    [add] update 커맨드 추가

--- cmd/update.go
+++ cmd/update.go
@@ -1,45 +1,23 @@
 package cmd
 
 import (
-	"github.com/dolong2/dcd-cli/api/exec"
-	cmdError "github.com/dolong2/dcd-cli/cmd/err"
-	"github.com/dolong2/dcd-cli/cmd/util"
+	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
-	Use:   "update <applicationId> [flags]",
-	Short: "use to update an application env",
-	Long:  `this command can be used to update a env to an application.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		workspaceId, err := util.GetWorkspaceId(cmd)
-		if err != nil {
-			return err
-		}
-
-		if len(args) == 0 {
-			return cmdError.NewCmdError(1, "must specify applicationId")
-		}
-		application := args[0]
-		key, existsKey := cmd.Flags().GetString("key")
-		value, existsValue := cmd.Flags().GetString("value")
-		if key == "" || value == "" || existsKey != nil || existsValue != nil {
-			return cmdError.NewCmdError(1, "this command needs to specify both and key and value")
-		}
-		err = exec.UpdateEnv(workspaceId, application, key, value)
-		if err != nil {
-			return cmdError.NewCmdError(1, err.Error())
-		}
-		return nil
+	Use:   "update",
+	Short: "command to update a resource",
+	Long:  `this command is used to update a resource`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("update called")
 	},
 }
 
 func init() {
-	envCmd.AddCommand(updateCmd)
+	rootCmd.AddCommand(updateCmd)
 
-	updateCmd.Flags().StringP("key", "k", "", "environment key")
-	updateCmd.Flags().StringP("value", "v", "", "environment value")
-	updateCmd.Flags().StringP("workspace", "w", "", "workspace id")
+	updateCmd.Flags().StringP("id", "", "", "resource id")
 }
