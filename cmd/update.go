68070485a93205c7bcbfabb781539ebb5b3ee8c1
dolong2 <k01066624566@gmail.com>
2024-07-15 23:10:33 +0900

    [add] update 메서드 호출 로직 추가

--- cmd/update.go
+++ cmd/update.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/dolong2/dcd-cli/api/exec"
+	cmdError "github.com/dolong2/dcd-cli/cmd/err"
 	"github.com/spf13/cobra"
 )
 
@@ -11,8 +11,34 @@ var updateCmd = &cobra.Command{
 	Use:   "update <resourceId>",
 	Short: "command to update a resource",
 	Long:  `this command is used to update a resource`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("update called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return cmdError.NewCmdError(1, "must enter both resource type and resource id")
+		}
+
+		resourceId := args[0]
+
+		fileDirectory, fileErr := cmd.Flags().GetString("file")
+		template, templateErr := cmd.Flags().GetString("template")
+		if fileErr != nil || templateErr != nil {
+			return cmdError.NewCmdError(2, "invalid flag")
+		}
+		if fileDirectory == "" && template == "" {
+			err := cmdError.NewCmdError(1, "there must be required either file flag or template flag")
+			return err
+		} else if fileDirectory != "" {
+			err := exec.UpdateByPath(resourceId, fileDirectory)
+			if err != nil {
+				return cmdError.NewCmdError(1, err.Error())
+			}
+		} else if template != "" {
+			err := exec.UpdateByTemplate(resourceId, template)
+			if err != nil {
+				return cmdError.NewCmdError(1, err.Error())
+			}
+		}
+
+		return nil
 	},
 }
 
