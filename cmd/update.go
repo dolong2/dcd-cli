6f2e64a97de217a618267d029ab43a3f356bd3be
dolong2 <k01066624566@gmail.com>
2024-08-20 21:17:26 +0900

    [move] put 파일에 있던 커맨드를 update 파일로 이동

--- cmd/update.go
+++ cmd/update.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"github.com/dolong2/dcd-cli/api/exec"
 	cmdError "github.com/dolong2/dcd-cli/cmd/err"
+	"github.com/dolong2/dcd-cli/cmd/util"
 	"github.com/spf13/cobra"
 )
 
@@ -42,9 +43,77 @@ var updateCmd = &cobra.Command{
 	},
 }
 
+// updateEnvCmd represents the put command
+var updateEnvCmd = &cobra.Command{
+	Use:   "update",
+	Short: "update an exists env",
+	Long:  `this command is used to update a exits global env`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		workspaceId, err := util.GetWorkspaceId(cmd)
+		if err != nil {
+			return err
+		}
+
+		if len(args) == 0 {
+			return cmdError.NewCmdError(1, "must specify applicationId")
+		}
+		application := args[0]
+		key, existsKey := cmd.Flags().GetString("key")
+		value, existsValue := cmd.Flags().GetString("value")
+		if key == "" || value == "" || existsKey != nil || existsValue != nil {
+			return cmdError.NewCmdError(1, "this command needs to specify both and key and value")
+		}
+		err = exec.UpdateEnv(workspaceId, application, key, value)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error())
+		}
+		return nil
+	},
+}
+
+// updateGlobalEnvCmd represents the put command
+var updateGlobalEnvCmd = &cobra.Command{
+	Use:   "update",
+	Short: "update a exists global env",
+	Long:  `this command is used to update a exits global env`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		workspaceId := ""
+
+		if len(args) == 0 {
+			var err error = nil
+			workspaceId, err = util.GetWorkspaceId(cmd)
+			if err != nil {
+				return err
+			}
+		} else {
+			workspaceId = args[0]
+		}
+
+		key, existsKey := cmd.Flags().GetString("key")
+		value, existsValue := cmd.Flags().GetString("value")
+		if key == "" || value == "" || existsKey != nil || existsValue != nil {
+			return cmdError.NewCmdError(1, "this command needs to specify both and key and value")
+		}
+		err := exec.UpdateGlobalEnv(workspaceId, key, value)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error())
+		}
+		return nil
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(updateCmd)
 
 	updateCmd.Flags().StringP("file", "f", "", "file path where a resource format is defined")
 	updateCmd.Flags().StringP("template", "", "", "resource template to json")
+
+	envCmd.AddCommand(updateEnvCmd)
+
+	updateEnvCmd.Flags().StringP("key", "", "", "select a key to delete")
+	updateEnvCmd.Flags().StringP("workspace", "w", "", "workspace id")
+
+	globalEnvCmd.AddCommand(updateGlobalEnvCmd)
+
+	updateGlobalEnvCmd.Flags().StringP("key", "", "", "select a key to delete")
 }
