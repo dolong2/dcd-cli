198c9ce6bd13641b67669a74a69e710a77a7432a
dolong2 <k01066624566@gmail.com>
2024-10-01 22:09:53 +0900

    [add] exec 명령에서 웹소켓을 통해서 컨테이너 내부에 접근할 수 있는 기능 추가

--- cmd/exec.go
+++ cmd/exec.go
@@ -1,11 +1,16 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/dolong2/dcd-cli/api/exec"
 	cmdError "github.com/dolong2/dcd-cli/cmd/err"
 	"github.com/dolong2/dcd-cli/cmd/util"
+	"github.com/dolong2/dcd-cli/websocket"
 	"github.com/spf13/cobra"
+	"os"
+	"os/signal"
+	"strings"
 )
 
 // execCmd represents the exec command
@@ -22,23 +27,80 @@ externally this command
 		}
 		applicationId := args[0]
 
-		workspaceId, err := util.GetWorkspaceId(cmd)
-		if err != nil {
-			return err
-		}
-
-		command, err := cmd.Flags().GetString("command")
+		ws, err := cmd.Flags().GetBool("ws")
 		if err != nil {
 			return cmdError.NewCmdError(1, err.Error())
 		}
 
-		cmdResult, err := exec.ExecCommand(workspaceId, applicationId, command)
-		if err != nil {
-			return cmdError.NewCmdError(1, err.Error())
-		}
+		if ws {
+			workingDir := "/"
+			conn, err := websocket.Connect(applicationId)
+			if err != nil {
+				return cmdError.NewCmdError(1, err.Error())
+			}
+			// 인터럽트 신호를 받기 위한 채널
+			interrupt := make(chan os.Signal, 1)
+			signal.Notify(interrupt, os.Interrupt)
 
-		for _, result := range cmdResult {
-			fmt.Println(result)
+			// 사용자 입력을 기다리며 메시지를 서버로 전송
+			reader := bufio.NewReader(os.Stdin)
+			for {
+				fmt.Print(workingDir + " > ")
+				input, _, _ := reader.ReadLine() // 사용자 입력 받기
+				err := websocket.SendMessage(conn, string(input))
+				if err != nil {
+					return cmdError.NewCmdError(1, err.Error())
+				}
+
+				// 인터럽트 신호 처리 (Ctrl+C)
+				select {
+				case <-interrupt:
+					err := websocket.Close(conn)
+					if err != nil {
+						return cmdError.NewCmdError(1, err.Error())
+					}
+					return nil
+				default:
+				}
+
+				for {
+					workingDirPrefix := "current dir = "
+					endPrefix := "cmd end"
+					message, err := websocket.ReadMessage(conn)
+					if err != nil {
+						return cmdError.NewCmdError(1, err.Error())
+					}
+					if strings.HasPrefix(message, workingDirPrefix) {
+						workingDir = strings.TrimPrefix(message, workingDirPrefix)
+						break
+					} else if strings.HasPrefix(message, endPrefix) {
+						break
+					}
+
+					fmt.Print(message)
+				}
+
+				fmt.Println("")
+			}
+		} else {
+			workspaceId, err := util.GetWorkspaceId(cmd)
+			if err != nil {
+				return err
+			}
+
+			command, err := cmd.Flags().GetString("command")
+			if err != nil {
+				return cmdError.NewCmdError(1, err.Error())
+			}
+
+			cmdResult, err := exec.ExecCommand(workspaceId, applicationId, command)
+			if err != nil {
+				return cmdError.NewCmdError(1, err.Error())
+			}
+
+			for _, result := range cmdResult {
+				fmt.Println(result)
+			}
 		}
 
 		return nil
@@ -50,4 +112,5 @@ func init() {
 
 	execCmd.Flags().StringP("command", "c", "", "a command to execute in an application")
 	execCmd.Flags().StringP("workspace", "w", "", "workspace id")
+	execCmd.Flags().Bool("ws", false, "apply websocket")
 }
