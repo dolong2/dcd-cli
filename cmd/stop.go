1e20952b49ae7559d3dbde10991226fa0619036d
dolong2 <k01066624566@gmail.com>
2024-10-27 16:24:19 +0900

    [add] 받은 커맨드로 애플리케이션을 정지하는 로직 추가

--- cmd/stop.go
+++ cmd/stop.go
@@ -19,6 +19,18 @@ var stopCmd = &cobra.Command{
 			return err
 		}
 
+		labels, err := cmd.Flags().GetStringArray("label")
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error())
+		}
+		if len(labels) != 0 {
+			err := exec.StopApplicationWithLabels(workspaceId, labels)
+			if err != nil {
+				return cmdError.NewCmdError(1, err.Error())
+			}
+			return nil
+		}
+
 		if len(args) == 0 {
 			return cmdError.NewCmdError(1, "must specify applicationId")
 		}
