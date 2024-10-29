ad6d02decb75aac42b665207ab8f5aff47402903
dolong2 <k01066624566@gmail.com>
2024-10-29 23:37:40 +0900

    [add] label을 받아서 해당 라벨이 포함된 애플리케이션에 환경변수를 추가하는 API 호출 메서드 추가

--- api/exec/add_env.go
+++ api/exec/add_env.go
@@ -3,6 +3,7 @@ package exec
 import (
 	"encoding/json"
 	"github.com/dolong2/dcd-cli/api"
+	"strings"
 )
 
 type envRequest struct {
@@ -23,6 +24,30 @@ func AddEnv(workspaceId string, applicationId string, key string, value string) 
 	requestJson, err := json.Marshal(envReq)
 
 	_, err = api.SendPost("/"+workspaceId+"/application/"+applicationId+"/env", header, map[string]string{}, requestJson)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func AddEnvWithLabels(workspaceId string, labels []string, key string, value string) error {
+	header := make(map[string]string)
+	accessToken, err := GetAccessToken()
+	if err != nil {
+		return err
+	}
+	header["Authorization"] = "Bearer " + accessToken
+
+	body := map[string]string{}
+	body[key] = value
+	envReq := envRequest{EnvList: body}
+	requestJson, err := json.Marshal(envReq)
+
+	params := make(map[string]string)
+	params["labels"] = strings.Join(labels, ",")
+
+	_, err = api.SendPost("/"+workspaceId+"/application/env", header, params, requestJson)
 	if err != nil {
 		return err
 	}
