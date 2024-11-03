01ba4fad6c6bab64d3883fdf0aab61e4a78a09bb
dolong2 <k01066624566@gmail.com>
2024-11-03 16:38:25 +0900

    [add] 라벨로 애플리케이션 환경변수를 수정하는 API 호출 메서드 추가

--- api/exec/update_env.go
+++ api/exec/update_env.go
@@ -3,6 +3,7 @@ package exec
 import (
 	"encoding/json"
 	"github.com/dolong2/dcd-cli/api"
+	"strings"
 )
 
 func UpdateEnv(workspaceId string, applicationId string, key string, value string) error {
@@ -21,7 +22,32 @@ func UpdateEnv(workspaceId string, applicationId string, key string, value strin
 	param := map[string]string{}
 	param["key"] = key
 
-	_, err = api.SendPatch("/"+workspaceId+"/application/"+applicationId+"/env?key="+key, header, map[string]string{}, requestJson)
+	_, err = api.SendPatch("/"+workspaceId+"/application/"+applicationId+"/env?key="+key, header, param, requestJson)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateEnvWithLabel(workspaceId string, labels []string, key string, value string) error {
+	header := make(map[string]string)
+	accessToken, err := GetAccessToken()
+	if err != nil {
+		return err
+	}
+	header["Authorization"] = "Bearer " + accessToken
+
+	body := map[string]string{}
+	body["newValue"] = value
+	envReq := envRequest{EnvList: body}
+	requestJson, err := json.Marshal(envReq)
+
+	param := map[string]string{}
+	param["key"] = key
+	param["labels"] = strings.Join(labels, ",")
+
+	_, err = api.SendPatch("/"+workspaceId+"/application/env?key="+key, header, param, requestJson)
 	if err != nil {
 		return err
 	}
