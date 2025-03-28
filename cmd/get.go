2de1f81ff0a37c66a435ef556fa664e47f4ea36b
dolong2 <k01066624566@gmail.com>
2025-03-28 23:53:45 +0900

    [chore] 내용 왼쪽 정렬 적용

--- cmd/get.go
+++ cmd/get.go
@@ -134,6 +134,7 @@ func init() {
 func printApplication(application exec.ApplicationResponse) {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetAutoWrapText(false)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
 
 	id := []string{"ID", application.Id}
 	name := []string{"Name", application.Name}
