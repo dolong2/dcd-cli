9535aa56172dad476d7e71ec5fcf7ffc3fcd3751
dolong2 <k01066624566@gmail.com>
2025-04-29 23:38:08 +0900

    [add] global env 템플릿 관련 구조체 추가

--- api/exec/create.go
+++ api/exec/create.go
@@ -80,6 +80,22 @@ type envPutListRequest struct {
 	EnvList []envPutRequest `json:"envList"`
 }
 
+type globalEnvTemplate struct {
+	Metadata metaData              `json:"metadata" yaml:"metadata"`
+	Spec     globalEnvSpecTemplate `json:"spec" yaml:"spec"`
+}
+
+type globalEnvSpecTemplate struct {
+	EnvList     []globalEnvListTemplate `json:"envList" yaml:"envList"`
+	WorkspaceId string                  `json:"workspaceId" yaml:"workspaceId"`
+}
+
+type globalEnvListTemplate struct {
+	Key        string `json:"key" yaml:"key"`
+	Value      string `json:"value" yaml:"value"`
+	Encryption bool   `json:"encryption" yaml:"encryption"`
+}
+
 type createWorkspaceResponse struct {
 	WorkspaceId string `json:"workspaceId"`
 }
