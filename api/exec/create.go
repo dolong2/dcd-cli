ee5aeb2c81041e7e9085d6117e02179eac13c6af
dolong2 <k01066624566@gmail.com>
2025-04-29 23:26:49 +0900

    [add] env 템플릿 관련 구조체 추가

--- api/exec/create.go
+++ api/exec/create.go
@@ -52,6 +52,23 @@ type applicationRequest struct {
 	Labels          []string `json:"labels"`
 }
 
+type envTemplate struct {
+	Metadata metaData        `json:"metadata" yaml:"metadata"`
+	Spec     envSpecTemplate `json:"spec" yaml:"spec"`
+}
+
+type envSpecTemplate struct {
+	EnvList       []envListTemplate `json:"envList" yaml:"envList"`
+	Labels        []string          `json:"labels" yaml:"labels"`
+	ApplicationId *string           `json:"applicationId,omitempty" yaml:"applicationId,omitempty"`
+}
+
+type envListTemplate struct {
+	Key        string `json:"key" yaml:"key"`
+	Value      string `json:"value" yaml:"value"`
+	Encryption bool   `json:"encryption" yaml:"encryption"`
+}
+
 type createWorkspaceResponse struct {
 	WorkspaceId string `json:"workspaceId"`
 }
