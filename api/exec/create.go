6ee7849cddf27b1295b9ccf939efb2d413c37646
dolong2 <k01066624566@gmail.com>
2025-05-01 21:13:41 +0900

    [refactor] 리소스 생성시 unmarshal 함수를 매개변수로 받도록 리펙토링

--- api/exec/create.go
+++ api/exec/create.go
@@ -22,12 +22,12 @@ func CreateByPath(fileDirectory string) error {
 	ext := filepath.Ext(fileDirectory)
 	switch ext {
 	case ".json":
-		resourceId, err = createByJson(content)
+		resourceId, err = create(content, json.Unmarshal)
 		if err != nil {
 			return err
 		}
 	case ".yml", ".yaml":
-		resourceId, err = createByYml(content)
+		resourceId, err = create(content, yaml.Unmarshal)
 		if err != nil {
 			return err
 		}
@@ -46,7 +46,7 @@ func CreateByPath(fileDirectory string) error {
 }
 
 func CreateByTemplate(rawTemplate string) error {
-	_, err := createByJson([]byte(rawTemplate))
+	_, err := create([]byte(rawTemplate), json.Unmarshal)
 	if err != nil {
 		return err
 	}
@@ -54,9 +54,9 @@ func CreateByTemplate(rawTemplate string) error {
 	return nil
 }
 
-func createByJson(content []byte) (string, error) {
+func create(content []byte, unmarshal func([]byte, interface{}) (err error)) (string, error) {
 	var data template.ParsingMetaData
-	err := json.Unmarshal(content, &data)
+	err := unmarshal(content, &data)
 	if err != nil {
 		return "", err
 	}
@@ -69,9 +69,10 @@ func createByJson(content []byte) (string, error) {
 	}
 
 	resourceType := data.Metadata.ResourceType
+
 	if resourceType == "WORKSPACE" {
 		var workspace template.WorkspaceTemplate
-		err = json.Unmarshal(content, &workspace)
+		err = unmarshal(content, &workspace)
 		if err != nil {
 			return "", err
 		}
@@ -96,144 +97,11 @@ func createByJson(content []byte) (string, error) {
 		if err != nil {
 			return "", err
 		}
+
 		return createWorkspaceResponse.WorkspaceId, nil
 	} else if resourceType == "APPLICATION" {
 		var application template.ApplicationTemplate
-		err = json.Unmarshal(content, &application)
-		if err != nil {
-			return "", err
-		}
-
-		err = application.ValidateMetadata()
-		if err != nil {
-			return "", err
-		}
-
-		request, err := json.Marshal(application.ToRequest())
-		if err != nil {
-			return "", err
-		}
-
-		workspaceId, err := getWorkspaceId()
-		if err != nil {
-			return "", err
-		}
-
-		result, err := api.SendPost("/"+workspaceId+"/application", header, map[string]string{}, request)
-		if err != nil {
-			return "", err
-		}
-		createApplicationResponse := response.CreateApplicationResponse{}
-		err = json.Unmarshal(result, &createApplicationResponse)
-		if err != nil {
-			return "", err
-		}
-		return createApplicationResponse.ApplicationId, nil
-	} else if resourceType == "ENV" {
-		var envTemplate template.EnvTemplate
-		err := json.Unmarshal(content, &envTemplate)
-		if err != nil {
-			return "", err
-		}
-
-		request, err := json.Marshal(envTemplate.ToRequest())
-
-		if err != nil {
-			return "", err
-		}
-
-		workspaceId, err := getWorkspaceId()
-		if err != nil {
-			return "", err
-		}
-
-		if envTemplate.Spec.Labels == nil && envTemplate.Spec.ApplicationId == nil {
-			return "", errors.New("애플리케이션 아이디 혹은 라벨이 입력되어야함")
-		} else if envTemplate.Spec.Labels != nil {
-			param := map[string]string{"labels": strings.Join(envTemplate.Spec.Labels, ",")}
-
-			_, err := api.SendPut("/"+workspaceId+"/application/env", header, param, request)
-			if err != nil {
-				return "", err
-			}
-		} else if envTemplate.Spec.ApplicationId != nil {
-			applicationId := *envTemplate.Spec.ApplicationId
-			_, err := api.SendPut("/"+workspaceId+"/application/"+applicationId+"/env", header, map[string]string{}, request)
-			if err != nil {
-				return "", err
-			}
-		}
-
-		return "", nil
-	} else if resourceType == "GLOBAL_ENV" || resourceType == "GE" {
-		var globalEnvTemplate template.GlobalEnvTemplate
-		err := json.Unmarshal(content, &globalEnvTemplate)
-		if err != nil {
-			return "", err
-		}
-
-		request, err := json.Marshal(globalEnvTemplate.ToRequest())
-
-		if err != nil {
-			return "", err
-		}
-
-		_, err = api.SendPut("/workspace/"+globalEnvTemplate.Spec.WorkspaceId+"/env", header, map[string]string{}, request)
-		if err != nil {
-			return "", err
-		}
-
-		return "", nil
-	} else {
-		return "", errors.New("지원되지 않는 리소스 타입입니다.")
-	}
-}
-
-func createByYml(content []byte) (string, error) {
-	var data template.ParsingMetaData
-	err := yaml.Unmarshal(content, &data)
-	if err != nil {
-		return "", err
-	}
-
-	header := make(map[string]string)
-	token, err := GetAccessToken()
-	header["Authorization"] = "Bearer " + token
-	if err != nil {
-		return "", err
-	}
-
-	resourceType := data.Metadata.ResourceType
-	if resourceType == "WORKSPACE" {
-		var workspace template.WorkspaceTemplate
-		err = yaml.Unmarshal(content, &workspace)
-		if err != nil {
-			return "", err
-		}
-
-		err = workspace.ValidateMetadata()
-		if err != nil {
-			return "", err
-		}
-
-		request, err := json.Marshal(workspace.ToRequest())
-		if err != nil {
-			return "", err
-		}
-
-		result, err := api.SendPost("/workspace", header, map[string]string{}, request)
-		if err != nil {
-			return "", err
-		}
-		createWorkspaceResponse := response.CreateWorkspaceResponse{}
-		err = json.Unmarshal(result, &createWorkspaceResponse)
-		if err != nil {
-			return "", err
-		}
-		return createWorkspaceResponse.WorkspaceId, nil
-	} else if resourceType == "APPLICATION" {
-		var application template.ApplicationTemplate
-		err := yaml.Unmarshal(content, &application)
+		err := unmarshal(content, &application)
 		if err != nil {
 			return "", err
 		}
@@ -264,16 +132,16 @@ func createByYml(content []byte) (string, error) {
 		if err != nil {
 			return "", err
 		}
+
 		return createApplicationResponse.ApplicationId, nil
 	} else if resourceType == "ENV" {
 		var envTemplate template.EnvTemplate
-		err := yaml.Unmarshal(content, &envTemplate)
+		err := unmarshal(content, &envTemplate)
 		if err != nil {
 			return "", err
 		}
 
 		request, err := json.Marshal(envTemplate.ToRequest())
-
 		if err != nil {
 			return "", err
 		}
@@ -294,6 +162,7 @@ func createByYml(content []byte) (string, error) {
 			}
 		} else if envTemplate.Spec.ApplicationId != nil {
 			applicationId := *envTemplate.Spec.ApplicationId
+
 			_, err := api.SendPut("/"+workspaceId+"/application/"+applicationId+"/env", header, map[string]string{}, request)
 			if err != nil {
 				return "", err
@@ -303,13 +172,12 @@ func createByYml(content []byte) (string, error) {
 		return "", nil
 	} else if resourceType == "GLOBAL_ENV" || resourceType == "GE" {
 		var globalEnvTemplate template.GlobalEnvTemplate
-		err := yaml.Unmarshal(content, &globalEnvTemplate)
+		err := unmarshal(content, &globalEnvTemplate)
 		if err != nil {
 			return "", err
 		}
 
 		request, err := json.Marshal(globalEnvTemplate.ToRequest())
-
 		if err != nil {
 			return "", err
 		}
@@ -321,6 +189,6 @@ func createByYml(content []byte) (string, error) {
 
 		return "", nil
 	} else {
-		return "", errors.New("지원되지 않는 리소스 타입입니다.")
+		return "", errors.New("지원되지 않는 리소스 타입입니다")
 	}
 }
