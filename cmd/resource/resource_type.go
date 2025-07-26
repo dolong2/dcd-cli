a349b3ab6820b96290286b4336d19b945a2db432
dolong2 <k01066624566@gmail.com>
2025-07-26 17:36:07 +0900

    [feat] 리소스 타입 추가

---
+++ cmd/resource/resource_type.go
@@ -0,0 +1,37 @@
+package resource
+
+type Type string
+type definedType []string
+
+var (
+	Workspace       = definedType{"WORKSPACE", "workspace", "workspaces", "ws"}
+	Application     = definedType{"APPLICATION", "application", "applications", "app"}
+	Env             = definedType{"ENV", "environment", "environments", "env"}
+	GlobalEnv       = definedType{"GLOBAL_ENV", "global_env", "global_envs", "ge"}
+	Domain          = definedType{"DOMAIN", "domain", "domains", "dom"}
+	ApplicationType = definedType{"TYPE", "type", "types", "ts"}
+)
+
+var allResourceTypes = []definedType{Workspace, Application, Env, GlobalEnv, Domain}
+
+func (rt Type) IsValid() bool {
+	for _, definedResource := range allResourceTypes {
+		for _, availableStr := range definedResource {
+			if rt == Type(availableStr) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (rt Type) IsEqual(other definedType) bool {
+	for _, availableStr := range other {
+		if rt == Type(availableStr) {
+			return true
+		}
+	}
+
+	return false
+}
