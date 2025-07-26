691fc7bd36051043cbecddbd99659b3981a1a42b
dolong2 <k01066624566@gmail.com>
2025-07-26 20:50:03 +0900

    [chore] version 커맨드 설명 수정

--- cmd/version.go
+++ cmd/version.go
@@ -10,9 +10,9 @@ import (
 
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
-	Use:   "version <resourceType>",
-	Short: "특정 리소스 타입의 버전을 출력하는 커맨드",
-	Long:  `플래그로 입력된 리소스 타입의 버전을 출력합니다.`,
+	Use:   "version <applicationType>",
+	Short: "특정 애플리케이션 타입의 버전을 출력하는 커맨드",
+	Long:  `입력한 애플리케이션 타입에서 이용가능한 버전을 출력합니다.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 || args[0] == "" {
 			return cmdError.NewCmdError(1, "리소스 타입이 입력되지 않았습니다.")
