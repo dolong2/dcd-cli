cc156e673b9a77ffe2a9d5a60ff1c7c58e566d05
dolong2 <k01066624566@gmail.com>
2025-07-27 16:48:42 +0900

    [del] setDomain 요청 구조체 제거

--- api/exec/request/domain.go
+++ api/exec/request/domain.go
@@ -1,9 +1,5 @@
 package request
 
-type SetDomainRequest struct {
-	Domain string `json:"domain"`
-}
-
 type CreateDomainRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
