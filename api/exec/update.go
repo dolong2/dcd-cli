7eb2dbeef3222466e6e30f1430170d86978b1e0f
dolong2 <k01066624566@gmail.com>
2025-07-27 22:13:15 +0900

    [refac] update 메서드에서 템플릿을 request로 래핑하는 로직 수정

--- api/exec/update.go
+++ api/exec/update.go
@@ -99,12 +99,11 @@ func update(resourceId string, content []byte, unmarshal func([]byte, interface{
 			return err
 		}
 
-		err = workspace.ValidateMetadata()
+		updateWorkspaceRequest, err := workspace.ToRequest()
 		if err != nil {
 			return err
 		}
-
-		request, err := json.Marshal(workspace.ToRequest())
+		request, err := json.Marshal(updateWorkspaceRequest)
 		if err != nil {
 			return err
 		}
@@ -125,7 +124,8 @@ func update(resourceId string, content []byte, unmarshal func([]byte, interface{
 			return err
 		}
 
-		request, err := json.Marshal(application.ToRequest())
+		updateApplicationRequest, err := application.ToRequest()
+		request, err := json.Marshal(updateApplicationRequest)
 		if err != nil {
 			return err
 		}
