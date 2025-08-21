6860441cb2b7972f6264460371053627bb009a5e
dolong2 <k01066624566@gmail.com>
2025-08-21 23:17:03 +0900

    [del] 라벨을 통한 환경변수 제거 메서드 삭제

--- api/exec/delete_env.go
+++ api/exec/delete_env.go
@@ -23,23 +23,3 @@ func DeleteEnv(key string, workspaceId string, applicationId string) error {
 
 	return nil
 }
-
-func DeleteEnvWithLabels(key string, workspaceId string, labels []string) error {
-	header := make(map[string]string)
-	accessToken, err := GetAccessToken()
-	if err != nil {
-		return err
-	}
-	header["Authorization"] = "Bearer " + accessToken
-
-	param := make(map[string]string)
-	param["key"] = key
-	param["labels"] = strings.Join(labels, ",")
-
-	_, err = api.SendDelete("/"+workspaceId+"/application/env", header, param)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
