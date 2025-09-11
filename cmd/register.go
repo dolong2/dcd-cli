7cb87951c880fa7203ab12f7beb66e4f182eb0eb
dolong2 <k01066624566@gmail.com>
2025-09-11 23:35:47 +0900

    [refac] 회원가입 커맨드에서 이름과 이메일을 인자가 아닌, 커맨드 플로우에서 입력받도록 수정

--- cmd/register.go
+++ cmd/register.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/dolong2/dcd-cli/api/exec"
 	cmdError "github.com/dolong2/dcd-cli/cmd/err"
 	"github.com/spf13/cobra"
@@ -10,21 +11,18 @@ import (
 
 // registerCmd represents the register command
 var registerCmd = &cobra.Command{
-	Use:   "register <email> <name>",
+	Use:   "register",
 	Short: "유저 가입 커맨드",
 	Long:  `유저 회원가입을 진행하는 커맨드 입니다.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 2 {
-			return cmdError.NewCmdError(1, "이메일과 이름이 입력되지 않음")
+		var email string
+		cmd.Print("이메일: ")
+		_, err := fmt.Scanf("%s", &email)
+		if email == "" || err != nil {
+			return cmdError.NewCmdError(1, "올바른 이메일을 입력해주세요")
 		}
 
-		email := args[0]
-		name := args[1]
-		if email == "" || name == "" {
-			return cmdError.NewCmdError(1, "이메일과 이름이 입력되지 않음")
-		}
-
-		err := exec.SendAuthCode(email, "SIGNUP")
+		err = exec.SendAuthCode(email, "SIGNUP")
 		if err != nil {
 			return cmdError.NewCmdError(1, err.Error())
 		}
@@ -42,6 +40,13 @@ var registerCmd = &cobra.Command{
 		if err != nil {
 			cmd.Println(err.Error())
 			goto enterCode
+		}
+
+		var name string
+		cmd.Print("이름: ")
+		_, err = fmt.Scanf("%s", &name)
+		if err != nil {
+			return cmdError.NewCmdError(1, err.Error())
 		}
 
 		cmd.Print("패스워드: ")
